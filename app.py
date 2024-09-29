ca2da84a72ed5ff11d1b17d15f8b61aed317dedc
timsullivan92 <89842238+timsullivan92@users.noreply.github.com>
2024-09-29 12:03:03 -0400

    Initial progress on Part 2

--- app.py
+++ app.py
@@ -1,30 +1,152 @@
 # Import the dependencies.
-
+import numpy as np
+import sqlalchemy
+import datetime as dt
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
-# reflect the tables
-
+Base = automap_base()
+# reflect the tables 
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2017-08-23<br/>"
+        f"/api/v1.0/start_end"
+        
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all precipitation for last 12 months"""
+    
+    # Calculate the date one year from the last date in data set.
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Query measurement table for date, prcp
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= query_date).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of yearly_precipitation
+    yearly_precipitation = []
+    for date,prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        yearly_precipitation.append(precipitation_dict)
+
+    return jsonify(yearly_precipitation)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    # Query station table for station data
+    results = session.query(Station.id, Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_stations
+    all_stations = []
+    for id, name, station, latitude, longitude, elevation in results:
+        station_dict = {}
+        station_dict["id"] = id
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        all_stations.append(station_dict)
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return the temperatures for the most active station for last 12 months"""
+    
+    # Calculate the date one year from the last date in data set.
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Query measurement table for date, tobs
+    
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date >= query_date).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of active_yearly_temps
+    active_yearly_temps = []
+    for date,tobs in results:
+        active_temp_dict = {}
+        active_temp_dict["date"] = date
+        active_temp_dict["tobs"] = tobs
+        active_yearly_temps.append(active_temp_dict)
+
+    return jsonify(active_yearly_temps)
+
+@app.route("/api/v1.0/2017-08-23")
+def startdate():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return the temperatures for the most active station for last 12 months"""
+    
+    # Calculate the date one year from the last date in data set.
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Query measurement table for date, tobs
+    
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date >= query_date).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of active_yearly_temps
+    active_yearly_temps = []
+    for date,tobs in results:
+        active_temp_dict = {}
+        active_temp_dict["date"] = date
+        active_temp_dict["tobs"] = tobs
+        active_yearly_temps.append(active_temp_dict)
+
+    return jsonify(active_yearly_temps)
+
+if __name__ == '__main__':
+    app.run(debug=True)
